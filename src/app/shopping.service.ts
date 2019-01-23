4b3e4046497f908b8a80a51d3ba7cbc8869ac7da
thorhsu <thorshu@gmail.com>
2019-01-23 09:21:22 +0800

    Changed to getter

--- src/app/shopping.service.ts
+++ src/app/shopping.service.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Commodity } from './api.service';
+import { isNgTemplate } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingService {
   shoppingItems: Commodity[] = [];
-  total = 0;
+  get total() {
+    let _total = 0;
+    for(const item of this.shoppingItems) {
+      _total += item.price;
+    }
+    return _total;
+  }
 
   constructor() { }
 
   addToCart(item: Commodity) {
     this.shoppingItems.push(item);
     this.shoppingItems = [...this.shoppingItems];
-    this.total += item.price;
   }
 
   removeFromCart(id: string) {
     for (let i = 0; i < this.shoppingItems.length; i++) {
       if (this.shoppingItems[i].id === id) {
-        this.total -= this.shoppingItems[i].price;
         this.shoppingItems.splice(i, 1);
         this.shoppingItems = [...this.shoppingItems];
         break;
